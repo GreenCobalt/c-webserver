8ce4dd27b93a57c0f3714fc4703743230a9bf797
GreenCobalt <34927146+GreenCobalt@users.noreply.github.com>
2024-04-23 17:10:39 -0400

    remove definition from .h file, move to http_error.c

--- include/http_error.h
+++ include/http_error.h
@@ -3,21 +3,6 @@
 
 #include "include/http_def.h"
 
-const static struct
-{
-    int code;
-    response_info page;
-} __HTTP_ERROR_PAGES[] = {
-    {
-        404,
-        {.content = {.content = "404 Not Found", .mime_type = "text/plain; charset=utf-8", .size = 13}, .content_length = 13, .http_code = 404},
-    },
-    {
-        501,
-        {.content = {.content = "501 Not Implemented", .mime_type = "text/plain; charset=utf-8", .size = 19}, .content_length = 19, .http_code = 501},
-    },
-};
-
 response_info http_code_to_response(int code);
 
 #endif /* HTTP_ERROR_H_ */
--- src/http_error.c
+++ src/http_error.c
@@ -1,5 +1,20 @@
 #include "include/http_error.h"
 
+const static struct
+{
+    int code;
+    response_info page;
+} __HTTP_ERROR_PAGES[] = {
+    {
+        404,
+        {.content = {.content = "404 Not Found", .mime_type = "text/plain; charset=utf-8", .size = 13}, .content_length = 13, .http_code = 404},
+    },
+    {
+        501,
+        {.content = {.content = "501 Not Implemented", .mime_type = "text/plain; charset=utf-8", .size = 19}, .content_length = 19, .http_code = 501},
+    },
+};
+
 response_info http_code_to_response(int code)
 {
     for (int i = 0; i < (sizeof(__HTTP_ERROR_PAGES) / sizeof(__HTTP_ERROR_PAGES[0])); i++)
